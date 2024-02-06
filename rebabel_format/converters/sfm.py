957bb7a1eb2a26da41626eb22c4ee10b26ce1001
aconeil <aconeil@iu.edu>
2024-02-05 23:13:14 -0500

    sfm_reader_update

--- rebabel_format/converters/sfm.py
+++ rebabel_format/converters/sfm.py
@@ -25,47 +25,44 @@ class SFMReader(Reader):
 
     def process_sentence(self, lines):
         sent_id = self.create_unit('sentence')
+        words = []
+        morphemes = []
         idx = 0
+        mix = 0
         for l in lines:
             if l.startswith('\\ref'):
                 ref = l.split('.')[1]
                 self.ensure_feature('sentence', 'SFM', 'reference', 'str')
                 self.set_feature(sent_id, 'SFM', 'reference', ref)
             elif l.startswith('\\tx'):
-                wds = l.replace('\\tx','').replace('\s\s+',' ').strip().split(' ')
+                wds = l[3:].strip().split()
                 for word in wds:
                     word = word.strip()
                     wid = self.create_unit('word', parent=sent_id)
-                    self.set_feature(wid, 'SFM', 'index', idx)
-                    idx +=1
-                    self.ensure_feature('word', 'SFM', 'form', 'str')
-                    self.set_feature(wid, 'SFM', 'form', word)
+                    words.append(wid)
+                    self.ensure_feature('word', 'sentence', 'form', 'str')
+                    self.set_feature(wid, 'sentence', 'form', word)
             elif l.startswith('\\mb'):
                 breaks = l.replace('\\mb','').strip().split('&')
                 for mb in breaks:
-                    #It would probably be better to make the word the parent?
-                    mid = self.create_unit('morpheme', parent=sent_id)
-                    mb_seg = re.split(r'(?=\=|-)', mb)
+                    mid = self.create_unit('morpheme', parent=words[idx])
+                    idx +=1
+                    mb_seg = re.split(r'(?=-|\=)', mb)
                     for morph in mb_seg:
-                        #create a position index that references the word index and morpheme index
-                        position = str(breaks.index(mb))+"."+str(mb_seg.index(morph))
                         morph = morph.strip()
-                        self.ensure_feature('morpheme','SFM','index','str')
-                        self.set_feature(mid, 'SFM', 'index', position)
+                        morphemes.append(mid)
                         self.ensure_feature('morpheme','SFM','form','str')
-                        self.set_feature(mid, 'SFM', 'form', str(morph))
+                        self.set_feature(mid, 'SFM', 'form', morph)
             elif l.startswith('\\gl'):
                 m_glosses = l.replace('\\gl','').strip().split('&')
                 for mg in m_glosses:
-                    mgid = self.create_unit('morph_gloss', parent=sent_id)
                     mg_seg = re.split(r'(?=-|\=)', mg)
                     for gloss in mg_seg:
-                        position = str(m_glosses.index(mg))+"."+str(mg_seg.index(gloss))
                         gloss = gloss.strip()
-                        self.ensure_feature('morph_gloss', 'SFM', 'index', 'str')
-                        self.set_feature(mgid, 'SFM', 'index', position)
-                        self.ensure_feature('morph_gloss', 'SFM', 'form', 'str')
-                        self.set_feature(mgid, 'SFM', 'form', str(gloss))
+                        
+                        self.ensure_feature('morpheme', 'SFM', 'gls', 'str')
+                        self.set_feature(morphemes[0], 'SFM', 'gls', gloss)
+                        morphemes.pop(0)
             elif l.startswith('\\ft'):
                 translation = l.replace('\\ft', '').strip()
                 self.ensure_feature('sentence', 'SFM', 'translation', 'str')
