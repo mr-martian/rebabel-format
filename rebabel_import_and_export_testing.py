5f02d6290fd9e8f313a92c1113cd1b692d5eba47
joembarron <67911762+joembarron@users.noreply.github.com>
2024-09-11 21:38:04 -0400

    Add file to experiment with rebabel import and export functionality

---
+++ rebabel_import_and_export_testing.py
@@ -0,0 +1,29 @@
+import rebabel_format
+
+rebabel_format.load_processes(True)
+rebabel_format.load_readers(True)
+rebabel_format.load_writers(True)
+
+rebabel_format.get_process_parameters('export')
+
+rebabel_format.run_command('import', mode='conllu', db='temp.db', infiles=['rebabel_format/test/static/data/basic.conllu'])
+
+rebabel_format.run_command(
+  # export out.flextext from in.db
+  'export', mode='flextext', db='temp.db', outfile='out.flextext',
+  # making some adjustments to account for differences between
+  # CoNLL-U and FlexText
+  mappings=[
+    # use CoNLL-U sentence nodes where FlexText expects phrases
+    {'in_type': 'sentence', 'out_type': 'phrase'},
+    {'in_feature': 'UD:upos', 'out_feature': 'FlexText/en:pos'},
+    # use UD:lemma where FlexText wants FlexText/en:txt
+    {'in_feature': 'UD:lemma', 'out_feature': 'FlexText/en:txt'},
+  ],
+  # settings specific to the FlexText writer:
+  # the highest non-empty node will be the phrase
+  # (the CoNLL-U importer currently doesn't create paragraph and document nodes)
+  root='phrase',
+  # the morpheme layer will also be empty
+  skip=['morph'],
+)
