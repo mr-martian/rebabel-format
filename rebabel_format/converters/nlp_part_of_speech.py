0fd5db401485c6863e10a8c9ef46e7bb513fa585
joembarron <67911762+joembarron@users.noreply.github.com>
2024-09-12 21:47:25 -0400

    Add natural language processing part of speech converter file

---
+++ rebabel_format/converters/nlp_part_of_speech.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from rebabel_format.reader import XMLReader
+from rebabel_format.writer import Writer
+from rebabel_format.parameters import Parameter
+
+class NLPPartOfSpeechReader(XMLReader):
+    "docstring placeholder"
+    def read_file(self, fin):
+        "docstring placeholder"
+        pass
+
+    def iter_nodes(self, node, parent=None, idx=0):
+        "docstring placeholder"
+        pass
+
