9c4e42dfa05e3800a71e09c74faec44293913671
joembarron <67911762+joembarron@users.noreply.github.com>
2024-09-15 10:36:25 -0400

    Format nlp_part_of_speech.py, use 'nlp_pos' mode in run_command function

--- rebabel_format/converters/nlp_part_of_speech.py
+++ rebabel_format/converters/nlp_part_of_speech.py
@@ -2,46 +2,46 @@
 
 from rebabel_format.reader import LineReader
 
+
 class NLPPartOfSpeechReader(LineReader):
     """Read in a text file consisting of lines with a line number and a single word and its part of speech, separated by the ↑ delimiter.
-    
+
     Example file structure:
-    
+
     1 The↑DET
     2 dog↑NOUN
     3 barked↑VERB
     4 .↑PUNC
     """
-    
-    identifier = 'nlp_pos'
-       
+
+    identifier = "nlp_pos"
+
     def reset(self):
         self.word_idx = 0
-          
+
     def end(self):
         if self.id_seq:
-            self.set_type('sentence', 'sentence')
-            self.set_feature('sentence', 'meta', 'index', 'int',
-                             self.block_count)
+            self.set_type("sentence", "sentence")
+            self.set_feature("sentence", "meta", "index", "int", self.block_count)
         super().end()
 
     def process_line(self, line: str):
         """Process one word, part of speech pair at a time.
-        
+
         Positional arguments:
         line -- a word and its part of speech separated by the ↑ delimiter (ex: jumped↑VERB)
         """
-        split_line = line.strip().split(' ')
-        
+        split_line = line.strip().split(" ")
+
         line_number = split_line[0]
         word, part_of_speech = split_line[1].split("↑")
-        
-        self.set_type(line_number, 'word')
-        self.set_parent(line_number, 'sentence')
-        self.set_feature(line_number, 'UD', 'id', 'str', line_number)
-        
+
+        self.set_type(line_number, "word")
+        self.set_parent(line_number, "sentence")
+        self.set_feature(line_number, "UD", "id", "str", line_number)
+
         self.word_idx += 1
-        
-        self.set_feature(line_number, 'meta', 'index', 'int', self.word_idx)
-        self.set_feature(line_number, 'UD', 'form', 'str', word)
-        self.set_feature(line_number, 'UD', 'upos', 'str', part_of_speech)
+
+        self.set_feature(line_number, "meta", "index", "int", self.word_idx)
+        self.set_feature(line_number, "UD", "form", "str", word)
+        self.set_feature(line_number, "UD", "upos", "str", part_of_speech)
--- rebabel_import_and_export_testing.py
+++ rebabel_import_and_export_testing.py
@@ -4,26 +4,34 @@ rebabel_format.load_processes(True)
 rebabel_format.load_readers(True)
 rebabel_format.load_writers(True)
 
-rebabel_format.get_process_parameters('export')
-
-rebabel_format.run_command('import', mode='conllu', db='temp.db', infiles=['rebabel_format/test/static/data/basic.conllu'])
+rebabel_format.get_process_parameters("export")
 
 rebabel_format.run_command(
-  # export out.flextext from in.db
-  'export', mode='flextext', db='temp.db', outfile='out.flextext',
-  # making some adjustments to account for differences between
-  # CoNLL-U and FlexText
-  mappings=[
-    # use CoNLL-U sentence nodes where FlexText expects phrases
-    {'in_type': 'sentence', 'out_type': 'phrase'},
-    {'in_feature': 'UD:upos', 'out_feature': 'FlexText/en:pos'},
-    # use UD:lemma where FlexText wants FlexText/en:txt
-    {'in_feature': 'UD:lemma', 'out_feature': 'FlexText/en:txt'},
-  ],
-  # settings specific to the FlexText writer:
-  # the highest non-empty node will be the phrase
-  # (the CoNLL-U importer currently doesn't create paragraph and document nodes)
-  root='phrase',
-  # the morpheme layer will also be empty
-  skip=['morph'],
+    "import",
+    mode="nlp_pos",
+    db="temp.db",
+    infiles=["nlp_pos.txt"],
+)
+
+rebabel_format.run_command(
+    # export out.flextext from in.db
+    "export",
+    mode="flextext",
+    db="temp.db",
+    outfile="out.flextext",
+    # making some adjustments to account for differences between
+    # CoNLL-U and FlexText
+    mappings=[
+        # use CoNLL-U sentence nodes where FlexText expects phrases
+        {"in_type": "sentence", "out_type": "phrase"},
+        {"in_feature": "UD:upos", "out_feature": "FlexText/en:pos"},
+        # use UD:lemma where FlexText wants FlexText/en:txt
+        {"in_feature": "UD:form", "out_feature": "FlexText/en:txt"},
+    ],
+    # settings specific to the FlexText writer:
+    # the highest non-empty node will be the phrase
+    # (the CoNLL-U importer currently doesn't create paragraph and document nodes)
+    root="phrase",
+    # the morpheme layer will also be empty
+    skip=["morph"],
 )
