6134e3f7f8a77a9d7cd157be3a54a3eed662ee12
Daniel Swanson <awesomeevildudes@gmail.com>
2024-09-18 12:38:22 -0400

    fix(concordance): correct parameter lists
    also handle document boundaries, add some caching for performance, and
    add a test

--- rebabel_format/processes/concordance.py
+++ rebabel_format/processes/concordance.py
@@ -3,6 +3,8 @@
 from rebabel_format.process import SearchProcess
 from rebabel_format.parameters import Parameter, FeatureParameter
 
+from functools import lru_cache
+
 class Concordance(SearchProcess):
     '''Perform a concordance search'''
 
@@ -14,8 +16,12 @@ class Concordance(SearchProcess):
                       help='features of units from the query to label each output line with')
     print = FeatureParameter(help='feature to display for units in the concordance window')
 
+    @lru_cache
+    def get_children(db, uid, child_type):
+        return db.get_units(child_type, parent=uid)
+
     def get_child_bound(self, uid, child_type, bound, right):
-        children = self.db.get_units(child_type, parent=uid)
+        children = Concordance.get_children(self.db, uid, child_type)
         fid, ftype = self.db.get_feature(child_type, 'meta', 'index')
         idx = self.db.get_feature_values(children, fid)
         children.sort(key=lambda c: idx.get(c, bound))
@@ -28,23 +34,27 @@ class Concordance(SearchProcess):
                 if idx.get(u, bound) < bound:
                     return u
 
-    def get_edge(db, uid, child_type, right=True):
+    def get_edge(self, uid, child_type, right=True):
+        if uid is None:
+            return None
         import math
         bound = -math.inf if right else math.inf
         u = self.get_child_bound(uid, child_type, bound=bound, right=right)
-        if not u:
-            u = self.get_edge(self.get_next(db, uid, right), child_type, right)
+        if u is None:
+            u = self.get_edge(self.get_next(uid, right), child_type, right)
         return u
 
     def get_next(self, uid, right=True):
+        if uid is None:
+            return None
         utype = self.db.get_unit_type(uid)
         fid, ftype = self.db.get_feature(utype, 'meta', 'index')
         idx = self.db.get_feature_value(uid, fid)
         parent = self.db.get_parent(uid)
-        if not parent:
-            return
+        if parent is None:
+            return None
         ret = self.get_child_bound(parent, utype, idx, right)
-        if not ret:
+        if ret is None:
             ret = self.get_edge(self.get_next(parent, right), utype, right)
         return ret
 
@@ -58,7 +68,8 @@ class Concordance(SearchProcess):
             r = self.get_next(r, True)
             left.append(l)
             right.append(r)
-        return list(reversed(left)) + [uid] + right
+        ret = list(reversed(left)) + [uid] + right
+        return [u for u in ret if u is not None]
 
     def print_span(self, span):
         utype = self.db.get_unit_type(span[0])
@@ -66,5 +77,5 @@ class Concordance(SearchProcess):
         print(' '.join(str(self.db.get_feature_value(u, fid)) for u in span))
 
     def per_result(self, result):
-        self.print_label(result)
+        self.print_label(result, self.label)
         self.print_span(self.get_span(result[self.center], self.width))
