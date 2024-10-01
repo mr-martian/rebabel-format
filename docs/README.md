ed9627b6f37bf48ac968e683ad83526373283abd
Daniel Swanson <awesomeevildudes@gmail.com>
2024-10-01 11:50:19 -0400

    feat(reader): add `merge_on` parameter (closes #8)
    Merge units on a single feature, respecting unit type and parent-child
    relations. Note that this assumes input types are mapped appropriately.

---
+++ docs/README.md
@@ -0,0 +1,15 @@
+# reBabel Documentation
+
+## For Users
+
+- [Manipulating data](manipulating.md)
+
+## For Developers
+
+- Main classes
+  - [Reader](readers.md)
+  - [Writer](writers.md)
+  - [Process](processes.md)
+- Other things
+  - [Parameters](parameters.md)
+  - [Plugin architecture](plugins.md)
--- rebabel_format/reader.py
+++ rebabel_format/reader.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from rebabel_format.db import RBBLFile
+from rebabel_format.db import RBBLFile, WhereClause
 from rebabel_format.parameters import Parameter, process_parameters
+from rebabel_format.query import ResultTable
 import logging
 from collections import defaultdict
 
@@ -13,6 +14,8 @@ class ReaderError(Exception):
 class Reader:
     identifier = None
     parameters = {}
+
+    merge_on = Parameter(required=False, type=dict)
 
     def __init__(self, db, user, conf, kwargs):
         self.db = db
@@ -95,12 +98,102 @@ class Reader:
             self.db.check_type(ftype, value)
         self.features[unit_name][(feature, ftype)] = (value, confidence)
 
+    def _remap_features(self):
+        new_feats = defaultdict(dict)
+        for name, dct in self.features.items():
+            for (feature, ftype), val in dct.items():
+                m_key = (feature, self.types.get(name))
+                n_key = (feature, None)
+                feature = self.feature_map.get(m_key,
+                                               self.feature_map.get(n_key, feature))
+                new_feats[name][(feature, ftype)] = val
+        self.features = new_feats
+
     def finish_block(self, parent_if_missing=None, keep_uids=False):
         parent_type_if_missing = None
         if parent_if_missing is not None:
             parent_type_if_missing = self.db.get_unit_type(parent_if_missing)
 
+        self._remap_features()
         uids = self.uids.copy()
+
+        # node names which have been merged and thus need feature setting
+        # rather than feature creation
+        is_merged = set()
+
+        if self.merge_on:
+            # 1. Gather the values to merge on
+            # node type => feature value => [node name, ...]
+            merge_values = {k: defaultdict(list) for k in self.merge_on}
+            for name in self.id_seq:
+                typ = self.types.get(name)
+                if typ in merge_values:
+                    for feat, val in self.features[name].items():
+                        if feat[0] == self.merge_on[typ]:
+                            merge_values[typ][val[0]].append(name)
+
+            # 2. Query for existing nodes with matching features
+            # node name => [feature value, ...]
+            merge_possible = defaultdict(list)
+            for typ, val_map in merge_values.items():
+                table = ResultTable(self.db,
+                                    {'N': {
+                                        'type': typ,
+                                        'features': [{
+                                            'feature': self.merge_on[typ],
+                                            'value': sorted(val_map.keys()),
+                                        }],
+                                    }})
+                table.add_features('N', [self.merge_on[typ]])
+                for nodes, features in table.results():
+                    val = features[nodes['N']][self.merge_on[typ]]
+                    for name in val_map[val]:
+                        merge_possible[name].append(nodes['N'])
+
+            # 3. Extract all relations in file and DB
+            child_names = defaultdict(list)
+            all_merge = []
+            for name in self.id_seq:
+                if name not in merge_possible:
+                    continue
+                all_merge += merge_possible[name]
+                if name not in self.parents:
+                    continue
+                if self.parents[name] not in merge_possible:
+                    # TODO: what is parent is in self.uids?
+                    del merge_possible[name]
+                child_names[self.parents[name]].append(name)
+            self.db.execute_clauses('SELECT parent, child FROM relations',
+                                    WhereClause('child', all_merge),
+                                    WhereClause('parent', all_merge),
+                                    WhereClause('active', True),
+                                    WhereClause('isprimary', True))
+            child_ids = defaultdict(list)
+            for p, c in self.db.cur.fetchall():
+                child_ids[p].append(c)
+
+            # 4. Remove correspondences which don't match existing
+            # parent-child links
+            todo = sorted(merge_possible.keys())
+            while todo:
+                next_todo = []
+                for name in todo:
+                    pc = set()
+                    for n in merge_possible[name]:
+                        pc.update(child_ids[n])
+                    for ch in child_names[name]:
+                        update = set(merge_possible[ch]) & pc
+                        if len(update) < len(merge_possible[ch]):
+                            merge_possible[ch] = sorted(update)
+                            next_todo.append(ch)
+                todo = next_todo
+
+            # 5. Make the correspondences
+            for name, ids in merge_possible.items():
+                if ids:
+                    uids[name] = ids[0]
+                    is_merged.add(name)
+
         for name in self.id_seq:
             if name in uids:
                 continue
@@ -129,7 +222,7 @@ class Reader:
                      'isprimary': False, 'active': True, 'date': self.db.now()}
                 )
         self.db.cur.executemany(
-            'INSERT INTO relations(parent, parent_type, child, child_type, isprimary, active, date) VALUES(:parent, :parent_type, :child, :child_type, :isprimary, :active, :date)',
+            'INSERT OR IGNORE INTO relations(parent, parent_type, child, child_type, isprimary, active, date) VALUES(:parent, :parent_type, :child, :child_type, :isprimary, :active, :date)',
             parents,
         )
         self.parents = {}
@@ -137,14 +230,9 @@ class Reader:
 
         feature_ids = {}
         features = []
+        merge_features = []
         for name in self.id_seq:
             for (feature, ftype), (value, conf) in self.features[name].items():
-                m_key = (feature, self.types[name])
-                n_key = (feature, None)
-                if m_key in self.feature_map:
-                    feature = self.feature_map[m_key]
-                elif n_key in self.feature_map:
-                    feature = self.feature_map[n_key]
                 key = (feature, ftype, self.types[name])
                 if key in feature_ids:
                     fid = feature_ids[key]
@@ -153,15 +241,29 @@ class Reader:
                     feature_ids[key] = fid
                 if ftype == 'ref':
                     value = uids[value]
-                features.append({
+                dct = {
                     'unit': uids[name], 'feature': fid, 'value': value,
                     'user': self.user, 'date': self.db.now(),
                     'confidence': conf,
-                })
-        self.db.cur.executemany(
-            'INSERT INTO features(unit, feature, value, user, date, confidence) VALUES(:unit, :feature, :value, :user, :date, :confidence)',
-            features,
-        )
+                }
+                if name in is_merged:
+                    merge_features.append(dct)
+                else:
+                    features.append(dct)
+        if features:
+            self.db.cur.executemany(
+                'INSERT INTO features(unit, feature, value, user, date, confidence) VALUES(:unit, :feature, :value, :user, :date, :confidence)',
+                features,
+            )
+        if merge_features:
+            self.db.cur.executemany(
+                'UPDATE features SET value = :value, user = :user, confidence = :confidence, date = :date WHERE unit = :unit AND feature = :feature',
+                merge_features,
+            )
+            self.db.cur.executemany(
+                'INSERT OR IGNORE INTO features(unit, feature, value, user, confidence, date) VALUES(:unit, :feature, :value, :user, :confidence, :date)',
+                merge_features,
+            )
         self.features = defaultdict(dict)
 
         self.id_seq = []
