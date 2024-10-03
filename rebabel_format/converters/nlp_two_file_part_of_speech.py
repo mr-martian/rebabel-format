f8167491f865fc71a4e997b4cde9b719d94279a2
Elizabeth Thorner <73670368+ZeldaGaladriel@users.noreply.github.com>
2024-10-02 22:01:19 -0400

    Initial two file attempt

--- rebabel_format/converters/nlp_two_file_part_of_speech.py
+++ rebabel_format/converters/nlp_two_file_part_of_speech.py
@@ -5,19 +5,18 @@ from rebabel_format.reader import LineReader
 
 
 class NLPTwoFilePartOfSpeechReader(LineReader):
-    """Read in a text file.
-    Each line consists of a single sentence made up of words tagged with their parts of speech.
-    Each word is separated from its part of speech by a delimiter, by default "/".
-    The words are separated from each other by spaces.
+    """
 
-    Example file structure:
-
-    The/DET dog/NOUN barked/VERB ./PUNC
-    The/DET cat/NOUN meowed/VERB ./PUNC
     """
 
     identifier = "nlp_two_file_pos"
     delimiter = Parameter(default="/", type=str)
+    isPartOfSpeechFile = Parameter(type=bool)
+
+    if isPartOfSpeechFile:
+        feature = "UD:pos"
+    else:
+        feature = "UD:form"
 
     def reset(self):
         """placeholder docstring"""
@@ -27,28 +26,22 @@ class NLPTwoFilePartOfSpeechReader(LineReader):
         """placeholder docstring"""
         if self.id_seq:
             self.set_type("sentence", "sentence")
-            self.set_feature("sentence", "meta", "index", "int", self.block_count)
+            self.set_feature("sentence", "meta:index", "int", self.block_count)
         super().end()
 
     def process_line(self, line: str):
-        """Process one word, part of speech pair at a time.
-
-        Positional arguments:
-        line -- a word and its part of speech separated by a delimiter (ex: jumped/VERB)
+        """
         """
         split_line = line.strip().split(" ")
 
-        for index, word_part_of_speech_pair in enumerate(split_line):
-            word, part_of_speech = word_part_of_speech_pair.split(self.delimiter)
-
+        for index, string in enumerate(split_line):
             index_as_string = str(index + 1)
 
             self.set_type(index_as_string, "word")
             self.set_parent(index_as_string, "sentence")
-            self.set_feature(index_as_string, "UD", "id", "str", index_as_string)
+            self.set_feature(index_as_string, "UD:id", "str", index_as_string)
 
             self.word_idx += 1
 
-            self.set_feature(index_as_string, "meta", "index", "int", self.word_idx)
-            self.set_feature(index_as_string, "UD", "form", "str", word)
-            self.set_feature(index_as_string, "UD", "upos", "str", part_of_speech)
+            self.set_feature(index_as_string, "meta:index", "int", self.word_idx)
+            self.set_feature(index_as_string, self.feature, "str", string)
