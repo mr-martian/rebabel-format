7a3660e4b5a2e495481d1e663a409ee1a17c906f
Elizabeth Thorner <73670368+ZeldaGaladriel@users.noreply.github.com>
2024-10-10 22:15:10 -0400

    NLP 2 file import

---
+++ rebabel_format/converters/nlp_part_of_speech.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+from rebabel_format.parameters import Parameter
+from rebabel_format.reader import LineReader
+
+
+class NLPPartOfSpeechReader(LineReader):
+    """
+    Supports two different methods for importing part of speech data.
+
+    First method:
+        Import one text file, with a "combined" nlpFileType, where each line consists of a single sentence
+        made up of words tagged with their parts of speech.
+        Each word is separated from its part of speech by a delimiter, by default "/".
+        The word/pos pairs are separated from each other by spaces.
+
+        Example file structure:
+
+        The/DET dog/NOUN barked/VERB ./PUNC
+        The/DET cat/NOUN meowed/VERB ./PUNC
+
+    Second method:
+        Import two text files, one with the original language data and one with the parts of speech.
+        The language data file (with a "language" nlpFileType) and the part of speech file
+        (with a "pos" nlpFileType) need to be uploaded in two different imports.
+        Either the language data or the part of speech file can be uploaded first, but in either case
+        the second import needs to include the merge_on parameter.
+        What a line consists of depends on the type of file. For a language data file, each line is a sentence
+        made up of words and punctuation separated by spaces. For a part of speech file, each line
+        is made up of parts of speech separated by spaces, corresponding to the words and punctuation
+        from the language data file.
+
+        Example file structure:
+
+        Language Data File -
+        The dog barked .
+        The cat meowed .
+
+        Part of Speech File -
+
+        DET NOUN VERB PUNC
+        DET NOUN VERB PUNC
+    """
+
+    identifier = "nlp_pos"
+    delimiter = Parameter(default="/", type=str)
+    include_boundaries = True
+    nlpFileType = Parameter(type=str, help="Must be 'language', 'pos', or 'combined'")
+
+    def __post_init__(self):
+        if self.nlpFileType not in ["language", "pos", "combined"]:
+            raise ValueError("nlpFileType parameter must be 'language', 'pos', or 'combined'")
+
+    def is_boundary(self, line):
+        return True
+
+    def process_line(self, line: str):
+        """Process one line of the file at a time."""
+
+        for index, item in enumerate(line.split(), 1):
+            self.set_type(index, "word")
+            self.set_parent(index, "sentence")
+            self.set_feature(index, "meta:index", "int", index)
+
+            if self.nlpFileType == "combined":
+                word, part_of_speech = item.split(self.delimiter)
+                self.set_feature(index, "nlp:form", "str", word)
+                self.set_feature(index, "nlp:pos", "str", part_of_speech)
+            elif self.nlpFileType == "language":
+                self.set_feature(index, "nlp:form", "str", item)
+            else:
+                self.set_feature(index, "nlp:pos", "str", item)
+
+        if line:
+            self.set_type('sentence', 'sentence')
+            self.set_feature('sentence', 'meta:index', 'int', self.block_count + 1)
